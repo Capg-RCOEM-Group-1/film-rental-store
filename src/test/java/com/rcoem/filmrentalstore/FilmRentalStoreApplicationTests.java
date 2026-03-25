8fdcc9a07236970f32bea43bf6b738715c0d34a6
adityadaharwal963 <adityaprowork@gmail.com>
2026-03-25 15:07:57 +0530

    init commit

---
+++ src/test/java/com/rcoem/filmrentalstore/FilmRentalStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcoem.filmrentalstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmRentalStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
