8c4f2c68fb511ea306dbeb6466fbaf9607886087
adityadaharwal963 <adityaprowork@gmail.com>
2026-03-26 12:23:11 +0530

    update timestamp data type

--- src/main/java/com/rcoem/filmrentalstore/entities/Staff.java
+++ src/main/java/com/rcoem/filmrentalstore/entities/Staff.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.LastModifiedDate;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Entity
@@ -30,9 +31,9 @@ public class Staff {
     // Represent Status of Staff (Soft delete), Default True
     private Boolean active = true;
 
-    @Column(name = "last_update", columnDefinition = "TIMESTAMP")
     @UpdateTimestamp
-    private LocalDateTime lastUpdate;
+    @Column(name = "last_update", columnDefinition = "TIMESTAMP")
+    private Timestamp last_update;
 
     // To add BLOB Object picture
 }
