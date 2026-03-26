a87b9387ab63ec883397719cb63ad10a5634ff6b
adityadaharwal963 <adityaprowork@gmail.com>
2026-03-26 12:23:11 +0530

    update timestamp data type

--- src/main/java/com/rcoem/filmrentalstore/entities/Staff.java
+++ src/main/java/com/rcoem/filmrentalstore/entities/Staff.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.sql.Blob;
