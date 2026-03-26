f0357ac92de63191219945d45a1939e572aa7573
Ekansh10 <ekanshmahajan1010@gmail.com>
2026-03-26 17:03:12 +0530

    Added Rental Entity with relation attributes and Updated Customer with Staff attribute

--- src/main/java/com/rcoem/filmrentalstore/entities/Rental.java
+++ src/main/java/com/rcoem/filmrentalstore/entities/Rental.java
@@ -22,5 +22,26 @@ public class Rental {
     @Column(columnDefinition = "TIMESTAMP")
     private Timestamp returnDate;
 
+    @ManyToOne
+    @JoinColumn(name = "inventory_id")
+    private Inventory inventory;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "staff_id")
+    private Staff staff;
+
+    public Rental() {
+    }
+
+    public Rental(Timestamp returnDate, Inventory inventory, Customer customer, Staff staff) {
+        this.returnDate = returnDate;
+        this.inventory = inventory;
+        this.customer = customer;
+        this.staff = staff;
+    }
 
 }
