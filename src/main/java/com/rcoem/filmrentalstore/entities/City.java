46648010a2b389a41b2cd979c5c7b98bdd6abfcd
Anvi Mahajan <anvimahajan@Anvis-MacBook-Air.local>
2026-03-26 17:08:53 +0530

    Address city entity created

---
+++ src/main/java/com/rcoem/filmrentalstore/entities/City.java
@@ -0,0 +1,35 @@
+package com.rcoem.filmrentalstore.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "city")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "city_id")
+    private Long cityId;
+
+    @Column(name = "city")
+    private String city;
+
+    @UpdateTimestamp
+    @Column(columnDefinition = "TIMESTAMP", nullable = false)
+    private Timestamp lastUpdate;
+
+    
+}
