f3db2cb50ea5d838a09c2fc878d505a3b9cfacc2
adityadaharwal963 <adityaprowork@gmail.com>
2026-03-26 17:13:34 +0530

    added relationship customer->payment , rental->payment

--- src/main/java/com/rcoem/filmrentalstore/entities/Payment.java
+++ src/main/java/com/rcoem/filmrentalstore/entities/Payment.java
@@ -1,14 +1,18 @@
 package com.rcoem.filmrentalstore.entities;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
+@Entity
+@Data
 public class Payment {
 
     private Double amount;
@@ -23,4 +27,12 @@ public class Payment {
     @ManyToOne
     @JoinColumn(name = "staff_id")
     private Staff staff;
+
+    @ManyToOne
+    @JoinColumn(name = "rental_id")
+    private Rental rental;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
 }
