26674f3aee2426710ff6a18009ba9e457bac95a3
Ameya G07 <gadhikararg@gmail.com>
2026-03-30 11:32:24 +0530

    Intermediate update

--- src/main/java/com/rcoem/filmrentalstore/repository/StoreRepository.java
+++ src/main/java/com/rcoem/filmrentalstore/repository/StoreRepository.java
@@ -4,9 +4,9 @@ import com.rcoem.filmrentalstore.entities.Address;
 import com.rcoem.filmrentalstore.entities.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store,Byte> {
-
-    Optional<Store> findByAddress(Address testAddress);
+    List<Store> findByAddress(Address testAddress);
 }
