c7a2b76e2b23a3d9f80a4fceb8e3b962a0024f98
Pratham Agarwal <prathamagarwal815@gmail.com>
2026-03-31 15:11:39 +0530

    Adding a new test for retrieving films by category name

--- src/test/java/com/rcoem/filmrentalstore/api/FilmCategoryApiTest.java
+++ src/test/java/com/rcoem/filmrentalstore/api/FilmCategoryApiTest.java
@@ -1,24 +1,12 @@
 package com.rcoem.filmrentalstore.api;
 
-import com.rcoem.filmrentalstore.entities.Category;
-import com.rcoem.filmrentalstore.entities.Film;
-import com.rcoem.filmrentalstore.entities.Language;
-import com.rcoem.filmrentalstore.enums.Rating;
-import com.rcoem.filmrentalstore.repository.CategoryRepository;
-import com.rcoem.filmrentalstore.repository.FilmRepository;
-import com.rcoem.filmrentalstore.repository.LanguageRepository;
 import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-
-import java.math.BigDecimal;
-import java.util.HashSet;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,50 +17,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class FilmCategoryApiTest {
 
     @Autowired
-    private MockMvc mockMvc;
-
-   /* @Autowired
-    private FilmCategoryRepository filmCategoryRepository;
-*/
-    @Autowired
-    private FilmRepository filmRepository;
-
-    @Autowired
-    private CategoryRepository categoryRepository;
-    @Autowired
-    private LanguageRepository languageRepository;
-
-    private Film film;
-    private Category category;
-
-    @BeforeEach
-    void setup() {
-        Language language = new Language();
-        language.setName("Spanish");
-        languageRepository.save(language);
-
-        film = new Film();
-        film.setTitle("Inception");
-        film.setDescription("A mind-bending thriller");
-        film.setReleaseYear(2010);
-        film.setRentalDuration(7);
-        film.setRentalRate(BigDecimal.valueOf(15.0));
-        System.out.println("FINAL VALUE = " + film.getRentalRate());
-        film.setLength(148);
-        film.setReplacementCost(BigDecimal.valueOf(50.0));
-        film.setRating(Rating.PG_13);
-        film.setSpecialFeatures(new HashSet<>());
-        film.setLanguage(language);
-        film = filmRepository.save(film);
-
-        category = new Category();
-        category.setName("Action_" + System.currentTimeMillis());
-        category = categoryRepository.save(category);
-    }
-
+    MockMvc mockMvc;
 
     @Test
     void shouldReturnFilmById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/films/search/byCategory?name=Action&size=5"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReturnFilmByName() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/films/search/byCategory?name=Action&size=5"))
                 .andDo(print())
                 .andExpect(status().isOk());
