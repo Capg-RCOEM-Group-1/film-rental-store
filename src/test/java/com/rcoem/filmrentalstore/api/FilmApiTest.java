9390ce8b68db66c3f3d866709accb21ede5d36e7
MandarNalhe <mandarnalhe@gmail.com>
2026-03-31 16:11:54 +0530

    FilmApiTest Case fixed

--- src/test/java/com/rcoem/filmrentalstore/api/FilmApiTest.java
+++ src/test/java/com/rcoem/filmrentalstore/api/FilmApiTest.java
@@ -40,17 +40,16 @@ class FilmApiTest {
     @Autowired
     private LanguageRepository languageRepository;
 
-    private Short savedFilmId;
-    private Byte savedLanguageId;
+    private Film savedFilm;
+    private Language savedLanguage;
 
-
-    private void setUp() {
+    @BeforeEach
+    public void setUp() {
         // 1. Create and save Language
         Language language = new Language();
         language.setName("English");
         // Use saveAndFlush if using JpaRepository to ensure it's in the DB immediately
-        Language savedLanguage = languageRepository.saveAndFlush(language);
-        savedLanguageId = savedLanguage.getId();
+        savedLanguage = languageRepository.saveAndFlush(language);
 
         // 2. Create and save Film
         Film film = new Film();
@@ -71,8 +70,7 @@ class FilmApiTest {
         film.setLanguage(savedLanguage);
         film.setLastUpdate(new Timestamp(System.currentTimeMillis()));
 
-        Film savedFilm = filmRepository.saveAndFlush(film);
-        savedFilmId = savedFilm.getFilmId();
+        savedFilm = filmRepository.saveAndFlush(film);
     }
 
     @Test
@@ -83,8 +81,7 @@ class FilmApiTest {
 
     @Test
     void shouldReturnFilmById() throws Exception {
-        setUp();
-        mockMvc.perform(MockMvcRequestBuilders.get("/films/" + savedFilmId))
+        mockMvc.perform(MockMvcRequestBuilders.get("/films/" + savedFilm.getFilmId()))
                 .andDo(print()) 
                 .andExpect(status().isOk());
     }
@@ -97,7 +94,6 @@ class FilmApiTest {
 
     @Test
     void shouldCreateFilm() throws Exception {
-        setUp();
         String filmJson = """
                 {
                     "title": "Interstellar",
@@ -110,7 +106,7 @@ class FilmApiTest {
                     "rating": "PG",
                     "language": "/api/languages/%d"
                 }
-                """.formatted(savedLanguageId);
+                """.formatted(savedLanguage.getId());
 
         mockMvc.perform(MockMvcRequestBuilders.post("/films")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -120,7 +116,6 @@ class FilmApiTest {
 
 @Test
 void shouldPartiallyUpdateFilm() throws Exception {
-    setUp();
         String patchJson = """
             {
                 "title": "Inception Updated",
@@ -128,7 +123,7 @@ void shouldPartiallyUpdateFilm() throws Exception {
             }
             """;
 
-    mockMvc.perform(MockMvcRequestBuilders.patch("/films/" + savedFilmId)
+    mockMvc.perform(MockMvcRequestBuilders.patch("/films/" +savedFilm.getFilmId())
             .contentType(MediaType.APPLICATION_JSON)
             .content(patchJson))
             .andExpect(status().isNoContent());
@@ -136,7 +131,6 @@ void shouldPartiallyUpdateFilm() throws Exception {
 
     @Test
     void shouldSearchFilmByTitle() throws Exception {
-        setUp();
         mockMvc.perform(MockMvcRequestBuilders.get("/films/search/byTitle")
                 .param("title", "Inception"))
                 .andExpect(status().isOk())
