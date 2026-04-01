945a579c5a5eaac551e82efe7be9bbc87ce730a8
Ekansh10 <ekanshmahajan1010@gmail.com>
2026-04-01 19:19:00 +0530

    Updated Address and Added StoreInfo

--- src/main/java/com/rcoem/filmrentalstore/entities/Address.java
+++ src/main/java/com/rcoem/filmrentalstore/entities/Address.java
@@ -1,12 +1,16 @@
 package com.rcoem.filmrentalstore.entities;
 
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.sql.Time;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.hibernate.annotations.UpdateTimestamp;
@@ -40,7 +44,6 @@ public class Address {
     private String phone;
 
     @NotNull
-    @JsonIgnore
     @Column(name = "location", columnDefinition = "GEOMETRY")
     private Point location;
 
@@ -49,13 +52,13 @@ public class Address {
     private Timestamp lastUpdate;
 
     @NotNull
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "city_id")
     private City city;
 
-    @OneToMany(mappedBy = "address",fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "address")
     private List<Customer> customers;
 
-    @OneToMany(mappedBy = "address",fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "address")
     private List<Staff> staffs;
 }
